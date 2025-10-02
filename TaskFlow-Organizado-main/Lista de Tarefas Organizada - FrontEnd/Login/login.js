c3521d32d7007af50583501622829bc1088020a9
Igor Alves <igoralves5928@gmail.com>
2025-10-02 08:02:01 -0300

    Add files via upload

---
+++ TaskFlow-Organizado-main/Lista de Tarefas Organizada - FrontEnd/Login/login.js
@@ -0,0 +1,9 @@
+function loginUsuario(event) {
+  event.preventDefault();
+  alert("Login realizado com sucesso!");
+  window.location.href = "..";
+}
+
+function loginGoogle() {
+  alert("Login com Google simulação (tem que botar api cabaço).");
+}
