a9aa8348867a58c92eff44c10e5aa1aefcb59796
GHbellotto <guibellot121230@gmail.com>
2025-10-03 07:22:50 -0300

    Add files via upload

---
+++ TaskFlow-Organizado-main/Lista de Tarefas Organizada - FrontEnd/Pagina De Cadastro/Pagina De Cadastro/cadastro.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("form");
+const nomeUsuario = document.getElementById("nome");
+const senha = document.getElementById('senha');
+const email = document.getElementById("mail");
+const confirmarSenha = document;getElementById("confirmarSenha");
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    alert ("CADASTRADO COM SUCESSO!")
+})
+
+function checarImputUserName(params) {
+    
+}
